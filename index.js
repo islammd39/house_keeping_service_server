f62e36c17cb432753fe801bc6a35512ba95886ff
islammd39e <islammdrahat308@gmail.comm>
2022-11-10 00:27:17 +0600

    upload data and send clint side

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config()
 
-app.use(express())
 app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rbojs1n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(uri);
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run(){
+  try{
+     const serviceCollection = client.db("myServiceCollection").collection("services")
+     app.get("/services", async (req, res)=>{
+      const cursor = serviceCollection.find({})
+      const services =await cursor.toArray()
+      res.send(services)
+     })
+
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(error=>console.error(error))
+
+
+
+app.get("/", (req, res)=>{
+  res.send("my server is running")
 })
 
-// console.log(process.env.S3_BUCKET);
-// console.log(process.env.SECRET_KEY);
-
-const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.rbojs1n.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, ()=>{
+  console.log(`web server is running${port}`);
 })
